c821000f14df08ee5b1a8ea003a27290e205bcda
JFB <neurovortex@hotmail.com>
2024-04-25 12:57:15 -0400

    Define the props for the provider component

--- src/context/UserRepositoryProvider.tsx
+++ src/context/UserRepositoryProvider.tsx
@@ -4,7 +4,12 @@ import UserRepository from "../repositories/UserRepository";
 // Create a context with a default value or null
 export const UserRepositoryContext = React.createContext<UserRepository | null>(null);
 
-export const UserRepositoryProvider = ({ children }: any) => {
+// Define the props for the provider component
+type ProviderProps = {
+  children: React.ReactNode;
+};
+
+export const UserRepositoryProvider = ({ children }: ProviderProps) => {
   const userRepository = new UserRepository();
 
   return <UserRepositoryContext.Provider value={userRepository}>{children}</UserRepositoryContext.Provider>;
